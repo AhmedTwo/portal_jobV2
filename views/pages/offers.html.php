c7a075b33ec839dae7c4878d6537eeb7a11f8385
Mayalabeille95 <Seghiriahmed9@gmail.com>
2025-07-08 17:25:27 +0200

    rien changé

--- views/pages/offers.html.php
+++ views/pages/offers.html.php
@@ -21,7 +21,7 @@
     <table id="idTable">
         <thead>
             <tr>
-                <th>LOGO COMPAGNIE</th>
+                <th>LOGO SOCIETE</th>
                 <th>POSTE</th>
                 <th>CONTRAT</th>
                 <th>ACTION</th>
@@ -40,8 +40,8 @@
                     <td>
                         <!-- Détails -->
                         <a href="OfferDetails.php?id=<?= $offer['id'] ?>" title="Détails">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
-                            <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
                             </svg>
                         </a>
                         <?php if (isset($_SESSION['new_role']) && $_SESSION['new_role'] === 'admin'): ?>
